19ffad69b31abefcc74a4ef7570e31afa7319fd0
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-10-03 11:21:47 -0400

    Beautifying clang-format

--- src/Multi_array_py.cpp
+++ src/Multi_array_py.cpp
@@ -1,18 +1,21 @@
 #include "Multi_array_py.h"
 
 // CLASS MACROS
-#define PY_MULTI_ARRAY(Type,Dim)\
-	py::class_<Multi_array<Type,Dim>>( m , "Multi_array_"#Type"_"#Dim)\
-	.def( py::init(&Multi_array<Type,Dim>::numpy_copy), "numpy_array"_a.noconvert() ) \
-	.def("get_copy", 	( py::array_t<int8_t,py::array::c_style> (Multi_array<Type,Dim>::*)()) &Multi_array<Type,Dim>::copy_py )\
-	.def("get", 		( py::array_t<int8_t,py::array::c_style> (Multi_array<Type,Dim>::*)()) &Multi_array<Type,Dim>::move_py )\
-	;
-	
-void init_Multi_array(py::module &m)
-{
-	PY_MULTI_ARRAY(int8_t,1);
-	PY_MULTI_ARRAY(int8_t,2);
-	PY_MULTI_ARRAY(int8_t,3);
+#define PY_MULTI_ARRAY(Type, Dim)                                              \
+  py::class_<Multi_array<Type, Dim>>(m, "Multi_array_" #Type "_" #Dim)         \
+      .def(py::init(&Multi_array<Type, Dim>::numpy_copy),                      \
+           "numpy_array"_a.noconvert())                                        \
+      .def("get_copy", (py::array_t<int8_t, py::array::c_style>(               \
+                           Multi_array<Type, Dim>::*)()) &                     \
+                           Multi_array<Type, Dim>::copy_py)                    \
+      .def("get", (py::array_t<int8_t, py::array::c_style>(                    \
+                      Multi_array<Type, Dim>::*)()) &                          \
+                      Multi_array<Type, Dim>::move_py);
+
+void init_Multi_array(py::module &m) {
+  PY_MULTI_ARRAY(int8_t, 1);
+  PY_MULTI_ARRAY(int8_t, 2);
+  PY_MULTI_ARRAY(int8_t, 3);
 }
 
 // CLOSE MACRO SCOPES
--- src/python_submodule.cpp
+++ src/python_submodule.cpp
@@ -3,10 +3,9 @@
 // See Pybind11 FAQ «How can I reduce the build time ?» :
 // https://pybind11.readthedocs.io/en/stable/faq.html#how-can-i-reduce-the-build-time
 
-//Python Binding and Time_Quad class instances.
-PYBIND11_MODULE(Multi_array, m)
-{
-     m.doc() = "Performant MultiDimensionnal array for C++.\nIncluding features to facilitate binding to python.\n";
-	init_Multi_array(m);
+// Python Binding and Time_Quad class instances.
+PYBIND11_MODULE(Multi_array, m) {
+  m.doc() = "Performant MultiDimensionnal array for C++.\nIncluding features "
+            "to facilitate binding to python.\n";
+  init_Multi_array(m);
 }
-
